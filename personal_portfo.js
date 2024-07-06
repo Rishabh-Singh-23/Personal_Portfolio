e2188046b80af84c72744378dc048b59fd340460
Rishabh Singh <ris45817@gmail.com>
2024-07-06 21:49:39 +0530

    Add files via upload

---
+++ personal_portfo.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
